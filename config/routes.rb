4cb25bea187e0dd8ce0993a6eb23208d163043c9
Pasha <patrick.raza7@gmail.com>
2018-11-15 18:28:50 +0100

    toutes les views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   root to: 'home#index'
+  
   get 'home/index'
+  get 'home/index1'
+  get 'home/index2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
